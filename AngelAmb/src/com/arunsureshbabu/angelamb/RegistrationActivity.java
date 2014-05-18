7731bcd2be192dea13aaf8b2ebe43e1592f31e51
ARUN SURESH BABU <arunsureshbabu@gmail.com>
2014-05-18 06:43:04 +0530

    Fourth Push
    18/05/2014 06:45 AM
    Post Not working
    Navigation Ok

--- AngelAmb/src/com/arunsureshbabu/angelamb/RegistrationActivity.java
+++ AngelAmb/src/com/arunsureshbabu/angelamb/RegistrationActivity.java
@@ -1,5 +1,20 @@
 package com.arunsureshbabu.angelamb;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.R.string;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -7,36 +22,26 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends ActionBarActivity {
-
-	EditText username;
-	Button submit;
-	Button reset;
+	SharedPreferences prefs = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registration);
 
-		addListenerOnButton();
+		prefs = getSharedPreferences("com.mycompany.myAppName", MODE_PRIVATE);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-	}
-
-	private void addListenerOnButton() {
-		submit = (Button) findViewById(R.id.Submit_button);
-		reset = (Button) findViewById(R.id.Reset_button);
-		username = (EditText) findViewById(R.id.Username_editText);
-		System.out.println("Fine");
 	}
 
 	@Override
@@ -76,4 +81,120 @@ public class RegistrationActivity extends ActionBarActivity {
 		}
 	}
 
+	public void reset(View v) {
+		EditText Username = (EditText) findViewById(R.id.Username_editText);
+		Username.setText("");
+		EditText Password = (EditText) findViewById(R.id.Password_editText);
+		Password.setText("");
+		;
+		EditText ConfirmPassword = (EditText) findViewById(R.id.confirmPassord_editText);
+		ConfirmPassword.setText("");
+		Spinner UserType = (Spinner) findViewById(R.id.userType_spinner);
+		UserType.setSelection(0);
+		EditText email = (EditText) findViewById(R.id.Email_editText);
+		email.setText("");
+		EditText PhoneNum = (EditText) findViewById(R.id.PhoneNum_editText);
+		PhoneNum.setText("");
+		EditText FirstName = (EditText) findViewById(R.id.FirstName_editText);
+		FirstName.setText("");
+		EditText LastName = (EditText) findViewById(R.id.LastName_editText);
+		LastName.setText("");
+	}
+
+	public void submit(View v) {
+		EditText Username = (EditText) findViewById(R.id.Username_editText);
+		String username = Username.getText().toString();
+		EditText Password = (EditText) findViewById(R.id.Password_editText);
+		String password = Password.getText().toString();
+		EditText ConfirmPassword = (EditText) findViewById(R.id.confirmPassord_editText);
+		String confirmpassword = ConfirmPassword.getText().toString();
+		Spinner UserType = (Spinner) findViewById(R.id.userType_spinner);
+		String userType = UserType.getSelectedItem().toString();
+		EditText email = (EditText) findViewById(R.id.Email_editText);
+		String EMail = email.getText().toString();
+		EditText PhoneNum = (EditText) findViewById(R.id.PhoneNum_editText);
+		String phonenum = PhoneNum.getText().toString();
+		EditText FirstName = (EditText) findViewById(R.id.FirstName_editText);
+		String firstName = FirstName.getText().toString();
+		EditText LastName = (EditText) findViewById(R.id.LastName_editText);
+		String lastName = LastName.getText().toString();
+		String EmergencyContactNumber1 = "102";
+		String EmergencyContactNumber2 = "101";
+		String EmergencyContactNumber3 = "100";
+		String EmergencyEmail1 = "test1@gmail.com";
+		String EmergencyEmail2 = "test2@gmail.com";
+		String EmergencyEmail3 = "test3@gmail.com";
+		TextView msgTextField = (TextView) findViewById(R.id.RegLabel_textView);
+		if (password.equals(confirmpassword)) {
+			if (username.length() > 0) {
+				/*
+				 * HttpClient httpclient = new DefaultHttpClient(); HttpPost
+				 * httppost = new HttpPost(
+				 * "http://angelamb.antolabs.com/angelamb/user/");
+				 * 
+				 * try { List<NameValuePair> nameValuePairs = new
+				 * ArrayList<NameValuePair>( 12); nameValuePairs .add(new
+				 * BasicNameValuePair("name", username)); nameValuePairs.add(new
+				 * BasicNameValuePair("mail", EMail)); nameValuePairs.add(new
+				 * BasicNameValuePair( "field_first_name", firstName));
+				 * nameValuePairs.add(new BasicNameValuePair( "field_last_name",
+				 * lastName)); nameValuePairs.add(new BasicNameValuePair(
+				 * "field_user_phone", phonenum)); nameValuePairs.add(new
+				 * BasicNameValuePair( "field_patient_type", userType));
+				 * nameValuePairs.add(new BasicNameValuePair("field_phone1",
+				 * EmergencyContactNumber1)); nameValuePairs.add(new
+				 * BasicNameValuePair("field_phone2", EmergencyContactNumber2));
+				 * nameValuePairs.add(new BasicNameValuePair("field_phone3",
+				 * EmergencyContactNumber3)); nameValuePairs.add(new
+				 * BasicNameValuePair("field_email1", EmergencyEmail1));
+				 * nameValuePairs.add(new BasicNameValuePair("field_email2",
+				 * EmergencyEmail2)); nameValuePairs.add(new
+				 * BasicNameValuePair("field_email3", EmergencyEmail3));
+				 * httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				 * httpclient.execute(httppost); //msgTextField.setText(""); //
+				 * reset the message text field Toast.makeText(getBaseContext(),
+				 * "Sent", Toast.LENGTH_SHORT) .show(); } catch
+				 * (ClientProtocolException e) { e.printStackTrace(); } catch
+				 * (IOException e) { e.printStackTrace(); }
+				 */
+				Intent i;
+				switch (UserType.getSelectedItemPosition()) {
+				case 1:
+					prefs.edit().putBoolean("isEmergencyCrew", true).commit();
+					i = new Intent(RegistrationActivity.this,
+							EmergencyCrewActivity.class);
+					startActivity(i);
+					System.out.println("Emergency Crew");
+					// Close this Activity.
+					finish();
+					break;
+				case 2:
+					prefs.edit().putBoolean("isDoctor", true).commit();
+					i = new Intent(RegistrationActivity.this,
+							DoctorActivity.class);
+					startActivity(i);
+					System.out.println("Doctor");
+					// Close this Activity.
+					finish();
+					break;
+				default:
+					prefs.edit().putBoolean("isGeneralUser", true).commit();
+					i = new Intent(RegistrationActivity.this,
+							GeneralUserActivity.class);
+					startActivity(i);
+					System.out.println("General User");
+					// Close this Activity.
+					finish();
+					break;
+				}
+			} else {
+				// display message if text field is empty
+				Toast.makeText(getBaseContext(), "All fields are required",
+						Toast.LENGTH_SHORT).show();
+			}
+		} else {
+			Toast.makeText(getBaseContext(), "Passwords not Match!",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
 }
